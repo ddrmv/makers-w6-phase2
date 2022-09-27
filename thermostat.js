ea068f860edc8bc0fb77ef3c0625a20eb879a9b5
D. Dramchev <ddramchev@gmail.com>
2022-09-27 23:41:47 +0100

    complete with refactor phase2 bite4 - a thermostat

---
+++ thermostat.js
@@ -0,0 +1,39 @@
+/* eslint-disable require-jsdoc */
+const DEFAULT_TEMPERATURE = 20;
+const PSM_ON_MAX_TEMP = 25;
+const PSM_OFF_MAX_TEMP = 32;
+const MIN_TEMP = 10;
+
+class Thermostat {
+  constructor() {
+    this.temperature = DEFAULT_TEMPERATURE;
+    this.powerSavingMode = false;
+  }
+
+  getTemperature() {
+    return this.temperature;
+  }
+
+  setPowerSavingMode(setting) {
+    this.powerSavingMode = setting;
+  }
+
+  up() {
+    if (this.powerSavingMode === true && this.temperature < PSM_ON_MAX_TEMP ||
+        this.powerSavingMode === false && this.temperature < PSM_OFF_MAX_TEMP) {
+      this.temperature++;
+    }
+  }
+
+  down() {
+    if (this.temperature > MIN_TEMP) {
+      this.temperature--;
+    }
+  }
+
+  reset() {
+    this.temperature = DEFAULT_TEMPERATURE;
+  }
+}
+
+module.exports = Thermostat;
---
+++ thermostat.test.js
@@ -0,0 +1,88 @@
+const Thermostat = require('./thermostat');
+
+describe('Thermostat', () => {
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
+
+  describe('#getTemperature', () => {
+    it('returns default value of 20', () => {
+      expect(thermostat.getTemperature()).toEqual(20);
+    });
+  });
+
+  describe('#up', () => {
+    it('increases temperature by 1', () => {
+      thermostat.up();
+      expect(thermostat.getTemperature()).toEqual(21);
+    });
+
+    it('increases up to 32 by default', () => {
+      for (let i = 0; i<20; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getTemperature()).toEqual(32);
+    });
+
+    it('increases up to 32 when powerSavingMode is false', () => {
+      thermostat.setPowerSavingMode(false);
+      repeatObjectFnTimes(thermostat, 'up', 20);
+      expect(thermostat.getTemperature()).toEqual(32);
+    });
+
+    it('increases up to 25 when powerSavingMode is true', () => {
+      thermostat.setPowerSavingMode(true);
+      repeatObjectFnTimes(thermostat, 'up', 20);
+      expect(thermostat.getTemperature()).toEqual(25);
+    });
+  });
+
+  describe('#down', () => {
+    it('decreases temperature by 1', () => {
+      thermostat.down();
+      expect(thermostat.getTemperature()).toEqual(19);
+    });
+
+    it('does not decrease temperature below 10', () => {
+      repeatObjectFnTimes(thermostat, 'down', 20);
+      expect(thermostat.getTemperature()).toEqual(10);
+    });
+  });
+
+  describe('#reset', () => {
+    it('resets temperature to 20', () => {
+      thermostat.down();
+      expect(thermostat.getTemperature()).toEqual(19);
+      thermostat.reset();
+      expect(thermostat.getTemperature()).toEqual(20);
+    });
+  });
+
+  describe('integration', () => {
+    it('passes the example test', () => {
+      expect(thermostat.getTemperature()).toEqual(20);
+      thermostat.up();
+      thermostat.up();
+      expect(thermostat.getTemperature()).toEqual(22);
+      thermostat.down();
+      expect(thermostat.getTemperature()).toEqual(21);
+      thermostat.setPowerSavingMode(true);
+      repeatObjectFnTimes(thermostat, 'up', 10);
+      expect(thermostat.getTemperature()).toEqual(25);
+      thermostat.setPowerSavingMode(false);
+      thermostat.up();
+      expect(thermostat.getTemperature()).toEqual(26);
+      thermostat.reset();
+      expect(thermostat.getTemperature()).toEqual(20);
+    });
+  });
+});
+
+
+const repeatObjectFnTimes = (object, callbackName, times) => {
+  for (let i = 0; i < times; i++) {
+    object[callbackName]();
+  }
+};
+
+
